fee3c998cc9d16391f0b2b1d3093492642d46c46
Yves Bolong <yve17@hotmail.be>
2023-11-17 15:13:42 +0100

    progressing

--- dream.php
+++ dream.php
@@ -11,9 +11,10 @@
 @import url(https://db.onlinewebfonts.com/c/5c1df21a40851df1d21f99888c7541f8?family=Londrina+Solid+Black);
 
         body {
-            background-image: url('togod.jpg');
-            background-repeat: no-repeat;
-            background-size: cover;
+            background-color: #B82C15;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3CradialGradient id='a' gradientUnits='objectBoundingBox'%3E%3Cstop offset='0' stop-color='%23FF7C24'/%3E%3Cstop offset='1' stop-color='%23B82C15'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='0' y1='750' x2='1550' y2='750'%3E%3Cstop offset='0' stop-color='%23dc541d'/%3E%3Cstop offset='1' stop-color='%23B82C15'/%3E%3C/linearGradient%3E%3Cpath id='s' fill='url(%23b)' d='M1549.2 51.6c-5.4 99.1-20.2 197.6-44.2 293.6c-24.1 96-57.4 189.4-99.3 278.6c-41.9 89.2-92.4 174.1-150.3 253.3c-58 79.2-123.4 152.6-195.1 219c-71.7 66.4-149.6 125.8-232.2 177.2c-82.7 51.4-170.1 94.7-260.7 129.1c-90.6 34.4-184.4 60-279.5 76.3C192.6 1495 96.1 1502 0 1500c96.1-2.1 191.8-13.3 285.4-33.6c93.6-20.2 185-49.5 272.5-87.2c87.6-37.7 171.3-83.8 249.6-137.3c78.4-53.5 151.5-114.5 217.9-181.7c66.5-67.2 126.4-140.7 178.6-218.9c52.3-78.3 96.9-161.4 133-247.9c36.1-86.5 63.8-176.2 82.6-267.6c18.8-91.4 28.6-184.4 29.6-277.4c0.3-27.6 23.2-48.7 50.8-48.4s49.5 21.8 49.2 49.5c0 0.7 0 1.3-0.1 2L1549.2 51.6z'/%3E%3Cg id='g'%3E%3Cuse href='%23s' transform='scale(0.12) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.2) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.25) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(0.3) rotate(-20)'/%3E%3Cuse href='%23s' transform='scale(0.4) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(0.5) rotate(20)'/%3E%3Cuse href='%23s' transform='scale(0.6) rotate(60)'/%3E%3Cuse href='%23s' transform='scale(0.7) rotate(10)'/%3E%3Cuse href='%23s' transform='scale(0.835) rotate(-40)'/%3E%3Cuse href='%23s' transform='scale(0.9) rotate(40)'/%3E%3Cuse href='%23s' transform='scale(1.05) rotate(25)'/%3E%3Cuse href='%23s' transform='scale(1.2) rotate(8)'/%3E%3Cuse href='%23s' transform='scale(1.333) rotate(-60)'/%3E%3Cuse href='%23s' transform='scale(1.45) rotate(-30)'/%3E%3Cuse href='%23s' transform='scale(1.6) rotate(10)'/%3E%3C/g%3E%3C/defs%3E%3Cg transform='rotate(0 0 0)'%3E%3Cg transform='rotate(0 0 0)'%3E%3Ccircle fill='url(%23a)' r='3000'/%3E%3Cg opacity='0.5'%3E%3Ccircle fill='url(%23a)' r='2000'/%3E%3Ccircle fill='url(%23a)' r='1800'/%3E%3Ccircle fill='url(%23a)' r='1700'/%3E%3Ccircle fill='url(%23a)' r='1651'/%3E%3Ccircle fill='url(%23a)' r='1450'/%3E%3Ccircle fill='url(%23a)' r='1250'/%3E%3Ccircle fill='url(%23a)' r='1175'/%3E%3Ccircle fill='url(%23a)' r='900'/%3E%3Ccircle fill='url(%23a)' r='750'/%3E%3Ccircle fill='url(%23a)' r='500'/%3E%3Ccircle fill='url(%23a)' r='380'/%3E%3Ccircle fill='url(%23a)' r='250'/%3E%3C/g%3E%3Cg transform='rotate(0 0 0)'%3E%3Cuse href='%23g' transform='rotate(10)'/%3E%3Cuse href='%23g' transform='rotate(120)'/%3E%3Cuse href='%23g' transform='rotate(240)'/%3E%3C/g%3E%3Ccircle fill-opacity='0.1' fill='url(%23a)' r='3000'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
             font-family: 'Poppins', sans-serif;
             margin: auto;
             
@@ -27,7 +28,7 @@
             
             text-align: center; 
         }
-        #principal, #resultat {
+        #principal {
             display: inline-block;
             width: 48%; /* Ajustez la largeur en fonction de vos besoins */
             vertical-align: top; /* Alignez le contenu en haut */
@@ -37,6 +38,20 @@
             display: table;
             clear: both;
         }
+       
+        #sele input {
+            display: inline;
+            width: 10%;
+            padding: 4px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+            border: 2px solid black;
+            border-radius: 25px;
+            
+            
+            
+        }
+        
 
         h1{
             font-family: "Fat Albert BT W01 Outline";
@@ -47,32 +62,64 @@
         form {
             font-family: "Londrina Solid Black";
             color: Black;
-            padding: 10px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            
+            padding: auto;
+            /* From https://css.glass */
+            background: rgba(255, 255, 255, 0.33);
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            backdrop-filter: blur(12px);
+            -webkit-backdrop-filter: blur(12px);
+            border: 1px solid rgba(255, 255, 255, 0.48);
         }
 
         label {
             display: block;
-            margin-bottom: 8px;
+            margin: auto;
+            margin-bottom: 3px;
+            font-size: 40px;
+            
         }
 
         input, select {
-            width: 50%;
-            padding: 8px;
+            width: 80%;
+            padding: 5px;
             margin-bottom: 16px;
             box-sizing: border-box;
+            border: 2px solid black;
+            border-radius: 4px;
+            background-color: rgba(255, 255, 255, 0.33);
+            font-size: 20px;
+            outline: none;
+            text-align: center;
+
+        }
+        video {
+            margin-top: 70px;
+            box-sizing: border-box;
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            backdrop-filter: blur(12px);
+            -webkit-backdrop-filter: blur(12px);
+
         }
 
         button {
             margin: auto;
             display: block;
-            background-color: #4caf50;
-            color: #fff;
+            
+            color: black;
             padding: 10px 20px;
             border: none;
-            border-radius: 4px;
+            border-radius: 25px;
             cursor: pointer;
+
+            background: rgba(255, 255, 255, 0.33);
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            backdrop-filter: blur(12px);
+            -webkit-backdrop-filter: blur(12px);
+            border: 1px solid rgba(255, 255, 255, 0.48);
         }
 
         button:hover {
@@ -88,7 +135,7 @@
         #texte {
             font-family: "Londrina Solid Black";
             color: Black;
-            font-size: 40px;
+            font-size: 25px;
             }
 
     </style>
@@ -98,39 +145,14 @@
 <div class="clearfix">
 <h1>Welcome to TOGO currency Converter</h1>
     <p id="texte"> Let me guess. You plan to visit our little country, and you want to know the value of your currency here? You're in the right place. Enjoy!</p>
-
+    <video width="640" height="360"  autoplay loop muted>
+        <source src="5459819-preview.mp4" type="video/mp4">
+    
+    </video>
 <div id="principal">
     
     <form action="" method="post">
-        <label for="amount">Amount:</label>
-        <input type="number" name="amount" required>
-
-        <label for="from_currency">Your Currency:</label>
-        <select name="from_currency" required>
-            <!-- Add options for different currencies -->
-            <option value="USD">USD</option>
-            <option value="EUR">EUR</option>
-            <option value="JPY">Japan (JPY)</option>
-            <option value="CNY">China (CNY)</option>
-            <option value="MAD">Morocco (MAD)</option>
-            <option value="GBP">England (GBP)</option>
-            <option value="MXN">Mexico (MXN)</option>
-            <option value="ARS">Argentina (ARS)</option>
-            <!-- Add more currencies as needed -->
-        </select>
-
-        <label for="to_currency">TOGO Local Currency:</label>
-        <select name="to_currency" required>
-            <option value="XOF">FCFA</option>
-        </select>
-
-        <button type="submit">Convert</button>
-
-    </form>
-
-    </div>
-    <div id="resultat">
-        <?php
+    <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $amount = $_POST["amount"];
             $base_currency = $_POST["from_currency"];
@@ -157,6 +179,37 @@
             }
         }
         ?>
+        
+        <div id="sele">
+
+        <label for="from_currency">Your Currency:</label>
+        <select name="from_currency" required>
+            <!-- Add options for different currencies -->
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="JPY">Japan (JPY)</option>
+            <option value="CNY">China (CNY)</option>
+            <option value="MAD">Morocco (MAD)</option>
+            <option value="GBP">England (GBP)</option>
+            <option value="MXN">Mexico (MXN)</option>
+            <option value="ARS">Argentina (ARS)</option>
+            <!-- Add more currencies as needed -->
+        </select>
+
+        <label for="to_currency">TOGO Local Currency:</label>
+        <select name="to_currency" required>
+            <option value="XOF">FCFA</option>
+        </select>
+        </div>
+        
+        <label for="amount">Amount:</label>
+        <input type="number" name="amount" required>
+
+        <button type="submit">Convert</button>
+
+    </form>
+
+   
 </div>
 </div>
 </body>
